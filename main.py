a929cd13a167398fdcdb6f73815a5e7773b92fee
etha1622 <114620543+etha1622@users.noreply.github.com>
2024-05-02 20:35:02 -0500

    Update main.py
    Updated with tabulate and complete table with example

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-# Week X: __________ program
+# Week 13: program 1 (Team)
 # Code by __________ (MM/DD/2024)
+
+import sqlite3
+from tabulate import tabulate
 
 def main():
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
     #I emailed the professor about what we should put down. Might have to look at next week to see what data we are dealing with
-    cur.execute('''CREATE TABLE Database (student_name NULL, student_id NULL,  course_name NULL, course_ID NULL, na NULL, na2 NULL, na3 NULL)''')
-    
-    for x in range(10):
-        cur.execute('INSERT INTO Database (?,?,?,?,?,?,?)')
-        
-    conn.commit()
-    
-    for row in cur.execute("SELECT * FROM Population"):
-        print(row)
+    cur.execute('''CREATE TABLE Database (student_name_last TEXT, student_name_first TEXT, student_id INTEGER, sutdent_year INTEGER, course_name TEXT, course_ID INTEGER, professor_name TEXT)''')
 
+    cur.execute('INSERT INTO Database VALUES ("Smith", "Ethan", 2048, 11, "Python", "2005", "McAnally")')
+    
+    # Select the data and extract results
+    cur.execute('SELECT * FROM Database')
+    headers = [desc[0] for desc in cur.description] # headers are the column headers
+    data = cur.fetchall() # data is the data in the table, excluding headers
+    
+    # Close the connection
     conn.close()
-
+    
+    # Put the results together as a tuple
+    results = (headers, data)
+    display_select_results(results)
+    
+def display_select_results(results):
+    # Use the tabulate package to print results
+    # Try using tablefmt="grid" and playing around with other parameters found online
+    print(tabulate(results[1], headers=results[0], tablefmt="fancy_outline", numalign="left"))
 
 if __name__ == '__main__':
     main()
