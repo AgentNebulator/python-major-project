5da92a7b8cb84f373306a1e036f244d7f64820ef
kbrozy18 <168654315+kbrozy18@users.noreply.github.com>
2024-05-07 20:16:52 -0500

    Added GUI to main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 # Week 14: Second program (Major Project Part 2)
-# Code by team of Mathias Laven, Ethan Smith, Kathleen Brozynski (05/06/2024)
+# Code by team of Mathias Laven, Ethan Smith, Kathleen Brozynski (05/07/2024)
 
 import sqlite3
 from tabulate import tabulate
 import student_data
+import tkinter
 
 
 def main():
@@ -30,11 +31,48 @@ def main():
 
     # Put the results together as a tuple
     results = (headers, data)
-    display_select_results(results)
+    data_table = display_select_results(results)
+
+    return data_table
+
+
+# Create Class for GUI
+class StudentDatabase:
+    # Use variable data_table from main() to show table
+    def __init__(self, data_table):
+        # Initiate main window, frames, and labels
+        self.main_window = tkinter.Tk()
+        self.main_window.title("Database")
+        self.top_frame = tkinter.Frame(self.main_window)
+        self.mid_frame = tkinter.Frame(self.main_window)
+        self.bottom_frame = tkinter.Frame(self.main_window)
+        self.value = tkinter.StringVar()
+
+        # Text is variable data_table to display table
+        self.data_label = tkinter.Label(self.top_frame,
+                                        text=data_table)
+
+        # Exit button for user-friendly exit
+        self.exit_button = tkinter.Button(self.mid_frame,
+                                          text='Exit',
+                                          command=self.main_window.destroy)
+
+        # Pack data
+        self.data_label.pack()
+        self.exit_button.pack(side='left')
+
+        self.top_frame.pack(padx=(40, 40), pady=(40, 10))
+        self.mid_frame.pack(padx=(40, 40), pady=(10, 10))
+        self.bottom_frame.pack(padx=(40, 40), pady=(10, 40))
+        tkinter.mainloop()
 
 
 def display_select_results(results):
     print(tabulate(results[1], headers=results[0], tablefmt="rounded_outline", numalign="left"))
+
+
+# Call both functions
+student_gui = StudentDatabase(main())
 
 
 if __name__ == '__main__':
