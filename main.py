bed1208002946889c9cef30aec69c8fe859084a6
AgentNebulator <mattylaven@gmail.com>
2024-05-09 12:15:51 -0500

    Added fetch button to get data (currently does nothing)

--- main.py
+++ main.py
@@ -41,7 +41,6 @@ class StudentDatabaseGUI:
     def __init__(self, results):
 
         self.__data = results
-        self.__data_table = display_select_results(results)
 
         # Initiate main window, frames, and labels
         self.__main_window = tkinter.Tk()
@@ -49,22 +48,31 @@ class StudentDatabaseGUI:
         self.__top_frame = tkinter.Frame(self.__main_window)
         self.__mid_frame = tkinter.Frame(self.__main_window)
         self.__bottom_frame = tkinter.Frame(self.__main_window)
-        self.__value = tkinter.StringVar()
+        self.__displayed_data = tkinter.StringVar()
 
         # Text is variable data_table to display table
         self.__data_label = tkinter.Label(self.__top_frame,
-                                        text=self.__data_table)
+                                          textvariable=self.__displayed_data)
 
+        self.__request_label = tkinter.Label(self.__mid_frame, text="Student ID: ")
         self.__request_entry = tkinter.Entry(self.__mid_frame)
+
+        self.__fetch_button = tkinter.Button(self.__bottom_frame,
+                                           text='Fetch',
+                                           command=self.__fetch_data)
 
         # Exit button for user-friendly exit
         self.__exit_button = tkinter.Button(self.__bottom_frame,
-                                          text='Exit',
-                                          command=self.__close_window)
+                                            text='Exit',
+                                            command=self.__close_window)
 
         # Pack data
         self.__data_label.pack()
+
+        self.__request_label.pack(side="left")
         self.__request_entry.pack()
+
+        self.__fetch_button.pack(side="left", padx="10")
         self.__exit_button.pack(side='left')
 
         self.__top_frame.pack(padx=(40, 40), pady=(40, 10))
@@ -72,6 +80,9 @@ class StudentDatabaseGUI:
         self.__bottom_frame.pack(padx=(40, 40), pady=(10, 40))
 
         tkinter.mainloop()
+
+    def __fetch_data(self):
+        pass
 
     def __close_window(self):
         # was experimenting and left this in
