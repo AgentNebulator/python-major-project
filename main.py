0a6f3f1c4760084947742ab2b9ff597a6742a0bc
AgentNebulator <mattylaven@gmail.com>
2024-05-09 13:28:33 -0500

    Added table to display full data as well

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sqlite3
 from tabulate import tabulate
 import student_data
 import tkinter
+import tkinter.ttk
 
 
 def main():
@@ -13,8 +14,8 @@ def main():
 
     # Create table
     cur.execute(
-        '''CREATE TABLE IF NOT EXISTS Students (student_id INTEGER PRIMARY KEY NOT NULL, student_name_last TEXT, 
-        student_name_first TEXT, student_year INTEGER, course_name TEXT, course_ID INTEGER, professor_name TEXT)'''
+        '''CREATE TABLE IF NOT EXISTS Students (student_id INTEGER PRIMARY KEY NOT NULL, last_name TEXT, 
+        first_name TEXT, school_year INTEGER, course_name TEXT, course_ID INTEGER, professor_name TEXT)'''
     )
 
     # Enter data
@@ -45,29 +46,49 @@ class StudentDatabaseGUI:
         # Initiate main window, frames, and labels
         self.__main_window = tkinter.Tk()
         self.__main_window.title("Database")
-        self.__top_frame = tkinter.Frame(self.__main_window)
-        self.__mid_frame = tkinter.Frame(self.__main_window)
-        self.__bottom_frame = tkinter.Frame(self.__main_window)
+
+        self.__treeview_frame = tkinter.Frame(self.__main_window)
+        self.__label_frame = tkinter.Frame(self.__main_window)
+        self.__entry_frame = tkinter.Frame(self.__main_window)
+        self.__button_frame = tkinter.Frame(self.__main_window)
+
+        self.__data_table = tkinter.ttk.Treeview(self.__treeview_frame)
+
+        self.__data_table['columns'] = self.__data[0]
+
+        self.__data_table.column("#0", width=0, stretch=False)
+        self.__data_table.heading("#0", anchor="center")
+
+        for header in self.__data[0]:
+            self.__data_table.column(header, anchor="center", width=100)
+            self.__data_table.heading(header, text=header, anchor="center")
+
+        for student in self.__data[1]:
+            self.__data_table.insert(parent='', index='end', values=student)
+
         self.__displayed_data = tkinter.StringVar()
+        self.__displayed_data.set("Request with Student ID to view data")
 
-        # Text is variable data_table to display table
-        self.__data_label = tkinter.Label(self.__top_frame,
-                                          textvariable=self.__displayed_data)
+        self.__data_label = tkinter.Label(self.__label_frame,
+                                          textvariable=self.__displayed_data,
+                                          relief="ridge")
 
-        self.__request_label = tkinter.Label(self.__mid_frame, text="Student ID: ")
-        self.__request_entry = tkinter.Entry(self.__mid_frame)
+        self.__request_label = tkinter.Label(self.__entry_frame, text="Student ID: ")
+        self.__request_entry = tkinter.Entry(self.__entry_frame)
 
-        self.__fetch_button = tkinter.Button(self.__bottom_frame,
+        self.__fetch_button = tkinter.Button(self.__button_frame,
                                              text='Fetch',
                                              command=self.__fetch_data)
 
         # Exit button for user-friendly exit
-        self.__exit_button = tkinter.Button(self.__bottom_frame,
+        self.__exit_button = tkinter.Button(self.__button_frame,
                                             text='Exit',
                                             command=self.__close_window)
 
         # Pack data
-        self.__data_label.pack()
+        self.__data_label.pack(ipadx=10, ipady=10)
+
+        self.__data_table.pack()
 
         self.__request_label.pack(side="left")
         self.__request_entry.pack()
@@ -75,23 +96,34 @@ class StudentDatabaseGUI:
         self.__fetch_button.pack(side="left", padx="10")
         self.__exit_button.pack(side='left')
 
-        self.__top_frame.pack(padx=(40, 40), pady=(40, 10))
-        self.__mid_frame.pack(padx=(40, 40), pady=(10, 10))
-        self.__bottom_frame.pack(padx=(40, 40), pady=(10, 40))
+        self.__treeview_frame.pack(padx=(40, 40), pady=(40, 10))
+        self.__label_frame.pack(padx=(40, 40), pady=(40, 10))
+        self.__entry_frame.pack(padx=(40, 40), pady=(10, 10))
+        self.__button_frame.pack(padx=(40, 40), pady=(10, 40))
 
         tkinter.mainloop()
 
     def __fetch_data(self):
+
         requested_id = self.__request_entry.get()
 
-        print(self.__data)
-        headers = self.__data[0]
         requested_data = [item for item in self.__data[1]
                           if (str(item[0]) == requested_id)][0]
 
+        formatted_data = {
+            "id": requested_data[0],
+            "last_name": requested_data[1],
+            "first_name": requested_data[2],
+            "grade_level": requested_data[3],
+            "class_name": requested_data[4],
+            "class_id": requested_data[5],
+            "class_teacher": requested_data[6]
+        }
+
         self.__displayed_data.set((
-            f"{requested_data[2]} {requested_data[1]}"
-            f""
+            f"Full Name: {formatted_data["first_name"]} {formatted_data["last_name"]} \n"
+            f"Grade level: {formatted_data["grade_level"]} \n"
+            f"Class: {formatted_data["class_teacher"]}'s {formatted_data["class_name"]} {formatted_data["class_id"]}"
         ))
 
     def __close_window(self):
@@ -110,5 +142,3 @@ def display_select_results(results):
 if __name__ == '__main__':
     # Call both functions
     student_gui = StudentDatabaseGUI(main())
-
-
